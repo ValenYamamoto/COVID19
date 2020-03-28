2a3c67d52041b29dad26e2df5c6232dc87a0dc09
Valen Yamamoto <valenryamamoto@gmail.com>
2020-03-27 18:56:45 -0700

    USA total added, new vs total cases added

--- process.py
+++ process.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from process_data import state_pop, state_den, StateDay
 from datetime import date
 from process_data import us_daily, italy_daily, italy_by_region
+from state_populations import abbr
 
 OPERATIONS = {"positive" : StateDay.get_positive,
               "negative": StateDay.get_negative,
@@ -12,6 +13,10 @@ OPERATIONS = {"positive" : StateDay.get_positive,
               "pending": StateDay.get_pending,
               "death": StateDay.get_death}
 
+class LogError(Exception):
+
+    def __init__(self, val: float):
+        self.val = val
 
 def make_date(d: str) -> date:
     return date(int(d[:4]), int(d[4:6]), int(d[6:8]))
@@ -20,9 +25,12 @@ def days_elapsed(d1: date, d2: date) -> int:
     return (d1 - d2).days
 
 def log(n):
-    if n == 0:
-        return math.log(0.000001)
-    return math.log(n)
+    try:
+        if n == 0:
+            return math.log(0.000001)
+        return math.log(n)
+    except ValueError:
+        raise LogError(n)
 
 def get_state_data(data: dict, state: str) -> dict:
     state_data = {}
@@ -131,9 +139,36 @@ def get_log_plot_data(start_date: date, state_data: dict, op: "func") -> ([], []
     plot_data = {days_elapsed(x, start_date): log(op(y)) for x, y in state_data.items() if x >= start_date and op(y) != -1}
     return list(plot_data.keys()), list(plot_data.values())
 
+def plot_states_new_vs_total_cases(start_date: str, *states: str) -> None:
+    """ New Confirmed Case v Total Confirmed Case, both log."""
+    start_date = date.fromisoformat(start_date)
+    for state in states:
+        try:
+            state_data = get_state_data(us_daily, state)
+            x, y = get_new_vs_total_cases_log_data(start_date, state_data)
+            plt.plot(x, y, label=state)
+            plt.scatter(x, y)
+        except LogError as e:
+            print(state, e.val)
+    plt.xlabel("Total Positive Cases (ln)")
+    plt.ylabel("New Positive Cases (ln)")
+     
+def get_new_vs_total_cases_log_data(start_date: date, state_data: dict) -> ([],[]):
+    plot_data = {log(y.positive): log(y.change) for x, y in state_data.items() if x >= start_date and y.change > 0 and y.positive != -1}
+    return list(plot_data.keys()), list(plot_data.values())
+    
 def plot_italy_positives_province(pro: str, start_date: str):
     start_date = date.fromisoformat(start_date)
 
+def plot_all_states(attr: str, start_date: str) -> None:
+    start_date = date.fromisoformat(start_date)
+    for state in abbr.values():
+        state_data = get_state_data(us_daily, state)
+        plt.scatter(*get_plot_data(start_date, state_data, OPERATIONS[attr]), label=state)
+
+def plot_all_states_new_vs_total(start_date: str) -> None:
+    plot_states_new_vs_total_cases(start_date, *tuple(abbr.values()))
+    
 def show_plot():
     plt.legend()
     plt.show()
--- process_data.py
+++ process_data.py
@@ -11,7 +11,7 @@ def make_date_not_iso(d: str) -> date:
 
 class StateDay:
 
-    def __init__(self, day, state, positive, negative=0, pending=0, death=0, total=0):
+    def __init__(self, day, state, positive=0, negative=0, pending=0, death=0, total=0):
         self.state = state
         self.day = day
         self.positive = self._to_numeric(positive)
@@ -19,6 +19,7 @@ class StateDay:
         self.pending = self._to_numeric(pending)
         self.death = self._to_numeric(death)
         self.total = self._to_numeric(total)
+        self.change = -1
         if state in state_pop:
             self.population = state_pop[state]
         else:
@@ -42,6 +43,18 @@ class StateDay:
 
     def get_total(self):
         return self.total
+
+    def update(self, state_day):
+        self.positive = self._aggr_total_clean(state_day.positive)
+        self.negative = self._aggr_total_clean(state_day.negative)
+        self.pending = self._aggr_total_clean(state_day.pending)
+        self.death = self._aggr_total_clean(state_day.death)
+        self.total = self._aggr_total_clean(state_day.total)
+        
+    def _aggr_total_clean(self, val):
+        if val == -1:
+            return 0
+        return val
     
     def _to_numeric(self, val):
         if val != '':
@@ -55,34 +68,50 @@ us_daily = {}
 
 with open('us_states_covid19_daily.csv') as file:
     reader = csv.reader(file)
+    last_total = False
     for row in reader:
         if row[0].isdigit():
             day = make_date(row[0])
+            state_day = StateDay(day, row[1], row[2], row[3], row[4], row[5], row[6])
+            if last_total != False:
+                state_day.change = state_day.positive - last_total
             if day in us_daily:
-                us_daily[day][row[1]] = StateDay(day, row[1], row[2], row[3], row[4], row[5], row[6])
+                us_daily[day][row[1]] = state_day
             else:
-                us_daily[day] = {row[1]: StateDay(day, row[1], row[2], row[3], row[4], row[5], row[6])}
+                us_daily[day] = {row[1]: state_day}
+            last_total = state_day.get_positive()
 
+
+last_positve = False
+for day in us_daily:
+    us_day = StateDay(day, "USA")
+    for state_day in us_daily[day].values():
+        us_day.update(state_day)
+    if last_total != False:
+        us_day.change = us_day.positive - last_positve
+    last_total = us_day.get_positive()
+    us_daily[day]["USA"] = us_day
+        
 italy_daily = {}
 italy_by_region ={}
-with open("covid19_italy_province.csv") as file:
-    reader = csv.reader(file)
-    for row in reader:
-        if row[0].isdigit():
-            day = date.fromisoformat(row[1].split()[0])
-            data = StateDay(day, row[6], row[10])
-            
-            if day in italy_daily:
-                italy_daily[day][row[6]] = [data]
-            else:
-                italy_daily[day] = {row[6]: data}
-
-            region = row[4]
-            
-            if region in italy_by_region:
-                if day in italy_by_region:
-                    italy_by_region[region][row[6]] = [data]
-                else:
-                    italy_by_region[region] = {row[6]: data}
-            else:
-                italy_by_region[region] = {day: {row[6]: data}}
+##with open("covid19_italy_province.csv") as file:
+##    reader = csv.reader(file)
+##    for row in reader:
+##        if row[0].isdigit():
+##            day = date.fromisoformat(row[1].split()[0])
+##            data = StateDay(day, row[6], row[10])
+##            
+##            if day in italy_daily:
+##                italy_daily[day][row[6]] = [data]
+##            else:
+##                italy_daily[day] = {row[6]: data}
+##
+##            region = row[4]
+##            
+##            if region in italy_by_region:
+##                if day in italy_by_region:
+##                    italy_by_region[region][row[6]] = [data]
+##                else:
+##                    italy_by_region[region] = {row[6]: data}
+##            else:
+##                italy_by_region[region] = {day: {row[6]: data}}
